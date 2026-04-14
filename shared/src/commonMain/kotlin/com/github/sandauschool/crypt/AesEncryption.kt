04d4a9ba3c56cf6f28de7ba93fb94474e3191eaf
purofle <purofle@gmail.com>
2026-04-15 00:28:47 +0800

    Refactor: Simplify iOS `desEncrypt` implementation using `refTo`

---
+++ shared/src/commonMain/kotlin/com/github/sandauschool/crypt/AesEncryption.kt
@@ -0,0 +1,3 @@
+package com.github.sandauschool.crypt
+
+expect fun desEncrypt(data: ByteArray, key: ByteArray, iv: ByteArray): ByteArray
--- shared/src/iosMain/kotlin/crypt/AesEncryption.ios.kt
+++ shared/src/iosMain/kotlin/crypt/AesEncryption.ios.kt
@@ -1,19 +1,18 @@
 package com.github.sandauschool.crypt
 
 import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.addressOf
-import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.alloc
+import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.ptr
-import kotlinx.cinterop.usePinned
+import kotlinx.cinterop.refTo
 import kotlinx.cinterop.value
-import platform.posix.size_tVar
 import platform.CoreCrypto.CCCrypt
 import platform.CoreCrypto.kCCAlgorithmDES
 import platform.CoreCrypto.kCCBlockSizeDES
 import platform.CoreCrypto.kCCEncrypt
 import platform.CoreCrypto.kCCOptionPKCS7Padding
 import platform.CoreCrypto.kCCSuccess
+import platform.posix.size_tVar
 
 @OptIn(ExperimentalForeignApi::class)
 actual fun desEncrypt(data: ByteArray, key: ByteArray, iv: ByteArray): ByteArray {
@@ -25,27 +24,19 @@ actual fun desEncrypt(data: ByteArray, key: ByteArray, iv: ByteArray): ByteArray
     return memScoped {
         val outMoved = alloc<size_tVar>()
 
-        val status = key.usePinned { keyPinned ->
-            iv.usePinned { ivPinned ->
-                data.usePinned { dataPinned ->
-                    output.usePinned { outPinned ->
-                        CCCrypt(
-                            op = kCCEncrypt,
-                            alg = kCCAlgorithmDES,
-                            options = kCCOptionPKCS7Padding,
-                            key = keyPinned.addressOf(0),
-                            keyLength = key.size.toULong(),
-                            iv = ivPinned.addressOf(0),
-                            dataIn = dataPinned.addressOf(0),
-                            dataInLength = data.size.toULong(),
-                            dataOut = outPinned.addressOf(0),
-                            dataOutAvailable = output.size.toULong(),
-                            dataOutMoved = outMoved.ptr
-                        )
-                    }
-                }
-            }
-        }
+        val status = CCCrypt(
+            op = kCCEncrypt,
+            alg = kCCAlgorithmDES,
+            options = kCCOptionPKCS7Padding,
+            key = key.refTo(0),
+            keyLength = key.size.toULong(),
+            iv = iv.refTo(0),
+            dataIn = data.refTo(0),
+            dataInLength = data.size.toULong(),
+            dataOut = output.refTo(0),
+            dataOutAvailable = output.size.toULong(),
+            dataOutMoved = outMoved.ptr
+        )
 
         if (status != kCCSuccess) {
             error("CCCrypt failed with status=$status")
